aad7ee8057f202bddc3b387fb1a01218be42da6b
MWayneMercado <MWayneMercado@gmail.com>
2022-06-09 16:43:50 -0700

    base duocontainer w input decorator and ng-content

--- src/app/components/duocontainer/duocontainer.component.ts
+++ src/app/components/duocontainer/duocontainer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-duocontainer',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./duocontainer.component.scss']
 })
 export class DuocontainerComponent implements OnInit {
+
+  @Input() imgSrc!: string;
 
   constructor() { }
 
