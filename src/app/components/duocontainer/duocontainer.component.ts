ad70995733b7dec1366f5243828d1e0588a8cd2c
MWayneMercado <MWayneMercado@gmail.com>
2022-06-10 13:28:35 -0700

    add img src change animations

--- src/app/components/duocontainer/duocontainer.component.ts
+++ src/app/components/duocontainer/duocontainer.component.ts
@@ -1,9 +1,18 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-duocontainer',
   templateUrl: './duocontainer.component.html',
-  styleUrls: ['./duocontainer.component.scss']
+  styleUrls: ['./duocontainer.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('* => *', [
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 }))
+      ]),
+    ])
+  ]
 })
 export class DuocontainerComponent implements OnInit {
 
